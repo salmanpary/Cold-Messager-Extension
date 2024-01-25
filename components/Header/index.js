f01ee2edee0caf2d807e7cc19e0165c8959d7fdd
1337kid <45532566+1337kid@users.noreply.github.com>
2024-01-25 18:08:24 +0530

    added buttons

--- components/Header/index.js
+++ components/Header/index.js
@@ -1,9 +1,13 @@
 import styles from './Header.module.css';
+import Button from '@mui/material/Button';
 
 export default function Header() {
   return (
     <>
-      <p className={styles.head}>Header / Menu / Navigation</p>
+      <div className={styles.navbar}>
+        <p className={styles.head}>Header / Menu / Navigation</p>
+        <Button variant="outlined">Login</Button>
+      </div>
     </>
   );
 }
--- components/Index/index.js
+++ components/Index/index.js
@@ -1,17 +1,15 @@
 import styles from '../../styles/Pages.module.css';
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
 
 export default function Index({ navigateToPage }) {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>NEXT-CHROME-STARTER</h1>
-        <p className={styles.description}>
-          This is an example of a Browser Extension built with NEXT.JS. Please
-          refer to the GitHub repo for running instructions and documentation
-        </p>
-        <h1 className={styles.code}>Index Page ./components/Index/index.js</h1>
-        <p>{"[ - This is Index page content - ]"}</p>
-        <p onClick={() => navigateToPage('new')}>{"Go to New Page >"}</p>
+      <ButtonGroup variant="contained" aria-label="outlined primary button group">
+        <Button>Saved Templates</Button>
+        <Button>New Template</Button>
+      </ButtonGroup>
       </main>
     </div>
   );
