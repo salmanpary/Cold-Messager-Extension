04cd4b1626ca5001a57091ac752897559de233de
salman pary <salmanpary@gmail.com>
2024-02-09 00:19:41 +0530

    fix:fixed the footer bug

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -3,24 +3,21 @@ import { FaLinkedin, FaTwitter, FaInstagram } from "react-icons/fa";
 export default function Footer() {
   return (
     <footer className={styles.footer}>
-      <h5>&copy; Cold Messager</h5>
+      <h5>&copy; <a href="https://www.coldmessager.com" target={"_blank"}>Cold Messager</a></h5>
 
       <div className={styles.footer__iconscontainer}>
           {/* LinkedIn Icon */}
           <a
             href="https://www.linkedin.com/company/cold-messager/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_companies%3BXVyiHlHhQL%2BQNt7Mz9jsEQ%3D%3D"
+            target={"_blank"}
             className={styles.footer__icons}
           >
-            <FaLinkedin className={styles.footer__icons_icon} onClick={
-              ()=>{
-            
-            window.href="https://www.linkedin.com/company/cold-messager/?lipi=urn%3Ali%3Apage%3Ad_flagship3_search_srp_companies%3BXVyiHlHhQL%2BQNt7Mz9jsEQ%3D%3D"
-              }
-            }/>
+            <FaLinkedin className={styles.footer__icons_icon} />
           </a>
           {/* Twitter Icon */}
           <a
             href="https://twitter.com/ColdM13955"
+            target={"_blank"}
             className={styles.footer__icons}
           >
             <FaTwitter className={styles.footer__icons_icon} />
@@ -28,6 +25,7 @@ export default function Footer() {
           {/* Instagram Icon */}
           <a
             href="https://www.instagram.com/coldmessager/"
+            target={"_blank"}
             className={styles.footer__icons}
           >
             <FaInstagram className={styles.footer__icons_icon} />
--- out/service_worker.js
+++ out/service_worker.js
@@ -1,18 +1,18 @@
-chrome.webNavigation.onDOMContentLoaded.addListener(function (details) {
+chrome?.webNavigation?.onDOMContentLoaded?.addListener(function (details) {
   chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
-    chrome.storage.local.get(["user"]).then(data => {
+    chrome?.storage?.local?.get(["user"])?.then(data => {
       let user = data
       if (!user) user={}
       chrome.tabs.sendMessage(tabs[0].id, { action: "runContentScript" , user: user})
-    })
+    }).catch(err => console.log(err))
   });
 });
 
 chrome.runtime.onMessage.addListener(
 async function(request, sender, sendResponse) {
   if (request.user) {
-      chrome.storage.local.set({'user': request.user})
-      .then(() => chrome.storage.local.get(["user"]))
+      chrome?.storage?.local?.set({'user': request.user})
+      .then(() => chrome.storage.local.get(["user"])).catch(err => console.log(err))
   }
 }
 );
